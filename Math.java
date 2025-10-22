47f5ea8d5e98476a5d68cc7946b84cccfe9f2784
Judy Alazemi <240006112@aston.ac.uk>
2025-10-22 13:19:06 +0100

    Implement Add function (Judy Alazemi 240006112)

--- Math.java
+++ Math.java
@@ -1,2 +1,13 @@
 public class Math{
+
+/*
+Developer: Judy Alazemi
+University ID: 240006112
+Function: This function takes two inputs as integers and returns the sum
+*/
+
+public int Add(int x, int y) {
+    return x + y;
+}
+
 }
