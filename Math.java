25d73d79b7987ba3f91297d77ad37762647b7986
Sara Ghareeb <230437205@aston.ac.uk>
2025-10-22 13:20:11 +0100

    Added Max function with developer info and comments

--- Math.java
+++ Math.java
@@ -1,2 +1,16 @@
 public class Math{
+
+    /********************************
+Developer: Sara Ghareeb
+University ID: 230437204
+Function: This function takes two integers as input and returns the maximum valaue
+********************************/
+public static int Max(int a, int b){
+    if (a > b){
+        return a;
+    
+    } else {
+        return b;
+    }
+}
 }
