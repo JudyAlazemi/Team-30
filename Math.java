07417a2a196d64413d39b7ef78190c546d1c96ee
Iman Javed <240120027@aston.ac.uk>
2025-10-22 16:20:34 +0100

    Implement Min Function

--- Math.java
+++ Math.java
@@ -1,2 +1,11 @@
 public class Math{
+    /********************************
+Developer: Iman Javed
+University ID: 240120027
+Function: This function takes two integer as input and returns the minimum one.
+********************************/
+public int Min(int a, int b) {
+    return (a <= b) ? a : b;
+}
+
 }
