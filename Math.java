4c8b5684a73b34488106e73a9a45db1c815977e0
chandni-chandni <240341105@aston.ac.uk>
2025-10-22 13:27:24 +0100

    Added multiplication function (Multi)

--- Math.java
+++ Math.java
@@ -1,2 +1,11 @@
-public class Math{
+public class Math {
+
+    /********************************
+    Developer: Chandni
+    University ID: 240341105
+    Function: This function takes two integers as input and returns their product.
+    ********************************/
+    int Multi(int x, int y) {
+        return x * y;
+    }
 }
