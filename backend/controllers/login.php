716378152cdf4d10a0cb4574627de15c70d36655
SaraGhareeb2304 <230437205@aston.ac.uk>
2025-11-25 13:31:32 +0000

    updated files

---
+++ backend/controllers/login.php
@@ -0,0 +1,39 @@
+<?php
+session_start();                    // strats a session to to store the users login data
+require_once "../config/db.php";   //imports db connection 
+
+$email = $_POST['email'] ?? '';
+$password = $_POST['password'] ?? '';
+
+// this checks if all fields are filled
+if (empty($email) || empty($password)) {
+    echo "Please fill in all fields";
+    exit;
+}
+
+$stmt = $conn->prepare("SELECT * FROM users WHERE email = ?");        //creates an SQL statement to search for the user by email
+$stmt->bind_param("s", $email);
+$stmt->execute();
+$result = $stmt->get_result();
+
+//checks if the email exists
+if ($result->num_rows === 1){
+    $user = $result->fetch_assoc();
+
+    if (password_verify($password, $user['password'])) {
+
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['user_name'] = $user['name'];
+
+        echo "Login successful! Welcome, " . $_SESSION['user_name'];
+        exit;
+    } else {
+        echo "Incorrect password.";
+    }
+
+} else {
+echo "Email not found.";
+
+}
+
+?>
