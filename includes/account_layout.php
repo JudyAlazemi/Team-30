29de71b309a4866766cb7fe19c74e7c58a9aaddc
Chandni <240341105@aston.ac.uk>
2026-02-13 17:27:21 +0000

    changes for dashboard

--- includes/account_layout.php
+++ includes/account_layout.php
@@ -3,7 +3,7 @@ if (session_status() === PHP_SESSION_NONE) session_start();
 require_once __DIR__ . "/../config/db.php";
 
 if (!isset($_SESSION['user_id'])) {
-  header("Location: login.html");
+  header("Location: /login.html");
   exit;
 }
 
